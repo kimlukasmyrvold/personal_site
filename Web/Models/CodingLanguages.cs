7604f0580789b11665b05ddd3798037f28483401
kimlukasmyrvold <kimlukasmyrvold06@gmail.com>
2024-05-02 02:04:53 +0200

    Some changes

---
+++ Web/Models/CodingLanguages.cs
@@ -0,0 +1,30 @@
+﻿namespace Web.Models;
+
+public class Certificate
+{
+    public string? Name { get; set; }
+    public string? Url { get; set; }
+    public string? ImageUrl { get; set; }
+    public string? ImageAlt { get; set; }
+    public string? Svg { get; set; }
+}
+
+public class Project
+{
+    public string? Name { get; set; }
+    public string? Url { get; set; }
+    public string? ImageUrl { get; set; }
+    public string? ImageAlt { get; set; }
+    public string? Svg { get; set; }
+}
+
+public class CodingLanguage
+{
+    public string? Name { get; set; }
+    public string? SubName { get; set; }
+    public string? ImageUrl { get; set; }
+    public string? ImageAlt { get; set; }
+    public string? Svg { get; set; }
+    public List<Certificate>? Certificates { get; set; }
+    public List<Project>? Projects { get; set; }
+}
