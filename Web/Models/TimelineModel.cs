e2240b60acc382aac54bea3b30dd550ff5a548ef
Kim Lukas Myrvold <kimlukasmyrvold06@gmail.com>
2024-06-20 02:41:18 +0200

    model for json timeline

---
+++ Web/Models/TimelineModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Web.Models;
+
+public class TimelineModel
+{
+    public string? Name { get; set; }
+    public string? StartTime { get; set; }
+    public string? EndTime { get; set; }
+    public string? Description { get; set; }
+    public string? ImageUrl { get; set; }
+    public string? Link { get; set; }
+}
