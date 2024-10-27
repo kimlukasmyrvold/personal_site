eb21070841a8b26b659bc9fcf42caf82182ae01a
kimlukasmyrvold <kimlukasmyrvold06@gmail.com>
2024-10-27 05:40:28 +0100

    update

--- Web/Models/GamesModel.cs
+++ Web/Models/GamesModel.cs
@@ -6,12 +6,6 @@ public class Platforms
     public string? Link { get; set; }
 }
 
-public class Image
-{
-    public string? Url { get; set; }
-    public string? Alt { get; set; }
-}
-
 public class Achievements
 {
     public string? Title { get; set; }
@@ -23,11 +17,18 @@ public class Achievements
 public class GamesModel
 {
     public string? Title { get; set; }
-    public string? Description { get; set; }
     public string? PlayTime { get; set; }
+    public string? LastUpdate { get; set; }
     public string? Purchased { get; set; }
     public List<Platforms>? Platforms { get; set; }
-    public Image? Banner { get; set; }
-    public List<Image>? Media { get; set; }
+    public string? Banner { get; set; }
     public List<Achievements>? Achievements { get; set; }
+}
+
+public enum SortOption
+{
+    Unlocked,
+    Locked,
+    NameAscending,
+    NameDescending
 }
