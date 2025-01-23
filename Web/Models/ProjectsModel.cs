f35ad594ac297756bde9a61362502d81db1bf5f9
kimlukasmyrvold <kimlukasmyrvold06@gmail.com>
2025-01-23 18:10:44 +0100

    Add Projects modal for future use

---
+++ Web/Models/ProjectsModel.cs
@@ -0,0 +1,26 @@
+﻿namespace Web.Models;
+
+public class Images
+{
+    public string? Alt { get; set; }
+    public string? Src { get; set; }
+}
+
+public class ProjectsModel
+{
+    public string? Name {get; set;}
+    public string? ShortDescription {get; set;}
+    public string? Description {get; set;}
+    public string? Website {get; set;}
+    public string? Github {get; set;}
+    public VisibilityOption? Visibility {get; set;}
+    public string? Frameworks {get; set;}
+    public string? Languages {get; set;}
+    public List<Images>? Images {get; set;}
+}
+
+public enum VisibilityOption
+{
+    Visible,
+    Hidden
+}
